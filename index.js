5ee043a1a223afa608b1cb4e7cd6e5bcb1a26dfb
Wagner Andrade <andradewagner@hotmail.com>
2020-03-13 17:06:57 -0300

    aprimoramento backend

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const fileUpload = require('express-fileupload')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const db = require('./db')
@@ -6,10 +7,12 @@ const testRouter = require('./routes/test-router')
 const app = express()
 const apiPort = 3001
 
+app.use(fileUpload())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
 app.use(bodyParser.json())
 
+db.on('connected', console.log.bind(console, 'Sucesso ao conectar ao MongoDB!'))
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 app.get('/', (req, res) => {
